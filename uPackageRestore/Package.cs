7c94ac4140a22771d75a4b58dc3fc639f897fd91
Michael Wulff Nielsen <mwulff@gmail.com>
2012-11-01 10:03:07 +0100

    Initial import

---
+++ uPackageRestore/Package.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using ICSharpCode.SharpZipLib.Zip;
+using System.Xml.Linq;
+
+namespace uPackageRestore
+{
+    class Package
+    {
+
+        public string PackageFolder { get; set; }
+
+
+        public XDocument PackageXml { get; set; }
+
+        public Package(string packageFolder)
+        {
+            PackageFolder = packageFolder;
+            if(!File.Exists(Path.Combine(PackageFolder, "package.xml")))
+                throw new Exception(string.Format("The folder '{0}' does not contain a package xml file.",packageFolder));
+
+            PackageXml = XDocument.Load(Path.Combine(PackageFolder, "package.xml"));
+        }
+
+
+        public void CopyPackageToDirectory(string path)
+        {
+            var files = PackageXml.Descendants("file");
+
+            int copied = 0;
+            int skipped = 0;
+
+            foreach (var file in files)
+            {
+                string src = file.Descendants("guid").First().Value;
+                string dest = file.Descendants("orgName").First().Value;
+                string destPath = file.Descendants("orgPath").First().Value;
+
+                if (destPath.StartsWith("/"))
+                    destPath = destPath.Substring(1);
+
+                destPath = destPath.Replace('/', '\\');
+
+
+                string finalDestination = Path.Combine(path, Path.Combine(destPath, dest));
+
+                if (File.Exists(finalDestination))
+                {
+                    skipped++;
+                    continue;
+                }
+
+                Console.WriteLine("Copy '{0}' to '{1}'", src, finalDestination);
+
+
+                if (!Directory.Exists(Path.GetDirectoryName(finalDestination)))
+                    Directory.CreateDirectory(Path.GetDirectoryName(finalDestination));
+
+                File.Copy(Path.Combine(PackageFolder, src), finalDestination);
+
+                copied++;
+
+            }
+
+            Console.WriteLine("Skipped {0} files, copied {1} files", skipped, copied);
+
+        }
+
+
+        public static Package FromWeb(string path)
+        {
+            string packageName = Program.PackageName;
+            string packageDir = Path.Combine(Program.WorkingDirectory, packageName);
+
+            if (!Directory.Exists(packageDir))
+            {
+
+                //Download and unpack
+                try
+                {
+                    WebClient client = new WebClient();
+
+                    string tmpfile = Path.GetTempFileName();
+
+                    client.DownloadFile(path, tmpfile);
+
+                    FileStream stream = new FileStream(tmpfile, FileMode.Open);
+
+                    DecompressToDirectory(stream, packageDir, "");
+
+
+                    File.Delete(tmpfile);
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+
+
+            return new Package(packageDir);
+        }
+
+
+        public static void DecompressToDirectory(Stream source, string targetPath, string pwd)
+        {
+            targetPath = Path.GetFullPath(targetPath);
+
+            using (ZipInputStream decompressor = new ZipInputStream(source))
+            {
+                if (!string.IsNullOrEmpty(pwd))
+                {
+                    decompressor.Password = pwd;
+                }
+
+                ZipEntry entry;
+
+                while ((entry = decompressor.GetNextEntry()) != null)
+                {
+
+                    string filePath = Path.Combine(targetPath, Path.GetFileName(entry.Name));
+
+                    string directoryPath = Path.GetDirectoryName(filePath);
+
+
+                    if (!string.IsNullOrEmpty(directoryPath) && !Directory.Exists(directoryPath))
+                    {
+                        Directory.CreateDirectory(directoryPath);
+                    }
+
+                    if (entry.IsDirectory)
+                    {
+                        continue;
+                    }
+
+                    byte[] data = new byte[2048];
+                    using (FileStream streamWriter = File.Create(filePath))
+                    {
+                        int bytesRead;
+                        while ((bytesRead = decompressor.Read(data, 0, data.Length)) > 0)
+                        {
+                            streamWriter.Write(data, 0, bytesRead);
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+}
---
+++ uPackageRestore/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace uPackageRestore
+{
+    class Program
+    {
+
+        public static string DestinationDirectory { get; protected set; }
+        public static string WorkingDirectory { get; protected set; }
+        public static string PackageName { get; set; }
+
+        static void Main(string[] args)
+        {
+
+
+            if (args.Length != 3)
+            {
+                Usage();
+                return;
+            }
+
+
+            //Check parameters
+            if (!Directory.Exists(args[1]))
+            {
+                Usage(string.Format("The directory '{0}' does not exist",args[1]));
+                return;
+            }
+
+            DestinationDirectory = args[1];
+            WorkingDirectory = Path.Combine(DestinationDirectory, ".uPackageRestore");
+
+            //Check if our cache directory exists
+            if (!Directory.Exists(WorkingDirectory))
+            {
+                try
+                {
+                    Directory.CreateDirectory(WorkingDirectory);
+                }
+                catch (Exception ex)
+                {
+                    Usage(ex.Message);
+                    return;                  
+                }
+            }
+
+            PackageName = args[2];
+
+            Package p = Package.FromWeb(args[0]);
+
+            p.CopyPackageToDirectory(DestinationDirectory);
+
+        }
+
+        private static void Usage(string extraMessage = "")
+        {
+            Console.WriteLine("Usage: uPackageRestore.exe <source_package_url> <destination_folder> <package_name>");
+            Console.WriteLine("");
+            if (extraMessage != "")
+            {
+                Console.WriteLine(extraMessage);
+                Console.WriteLine("");
+            }
+        }
+
+    }
+}
